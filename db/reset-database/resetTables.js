f632468f800e3ec8990b02444529761cc1ae8e3c
PurpleNurps <seanlunt1991@gmail.com>
2023-10-25 14:45:22 +0100

    added seedTable function

--- db/reset-database/resetTables.js
+++ db/reset-database/resetTables.js
@@ -1,1 +1,7 @@
 import { dropTables } from "./dropTables.js";
+import { seedTables } from "./seedTables.js";
+import { createTables } from "./createTables.js";
+
+dropTables();
+createTables();
+seedTables();
