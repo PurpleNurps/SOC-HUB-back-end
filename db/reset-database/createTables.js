b35bcb98604ad8f0d37bbe0e035d2f790905ee40
PurpleNurps <seanlunt1991@gmail.com>
2023-10-25 14:23:54 +0100

    added createTable function

--- db/reset-database/createTables.js
+++ db/reset-database/createTables.js
@@ -1,16 +1,20 @@
-import { pool } from '../index.js'
+import { pool } from "../index.js";
 
 export async function createTables() {
-    try {
-        await pool.query(`
-    CREATE TABLE IF NOT EXISTS resourses,
-    topic TEXT IDENTITY PRIMARY KEY NOT NULL,
-    id INT GENERATED ALWAYS,
-    name TEXT NOT NULL,
-    link TEXT NOT NULL
-    `)
-        console.log('resources table has been created successfully')
-    } catch (error) {
-        console.error('Error creating resourses, Error originated in createTables.js', error);
-    }
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS resources (
+            id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            topic TEXT NOT NULL,
+            name TEXT NOT NULL,
+            link TEXT NOT NULL
+        );
+    `);
+    console.log("resources table has been created successfully");
+  } catch (error) {
+    console.error(
+      "Error creating resourses, Error originated in createTables.js",
+      error
+    );
+  }
 }
