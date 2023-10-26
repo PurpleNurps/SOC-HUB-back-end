6e05c5000d739377f0b24402c71ede8900ef5096
PurpleNurps <seanlunt1991@gmail.com>
2023-10-26 12:25:35 +0100

    added fetch function

---
+++ fetch.js
@@ -0,0 +1,32 @@
+const PORT = process.env.PORT || 5555;
+const userInput = "";
+
+export async function fetchResources() {
+  // Make a HTTP GET request with the fetch method
+  // HTTP METHOD: GET
+  // Address: http://localhost:port
+  // Header: Accept: application/json
+
+  // Declare a variable to store the HTTP response
+
+  const response = await fetch(
+    `http://localhost:${PORT}/resources/${userInput}`,
+    {
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
+  // If the request fails log out an error
+  if (!response.ok) {
+    console.error(`Status: ${response.status}`);
+    console.error(`Text: ${await response.text()}`);
+    return;
+  }
+
+  // Use the json method that parses the reponses body as JSON.
+  const data = await response.json();
+
+  // Log out our parsed data
+  console.log(typeof data, data);
+}
