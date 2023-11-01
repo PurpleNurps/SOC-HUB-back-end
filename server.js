1fdc9c9297daf362147c2932036b3d1effca6373
Alex321111 <139920296+Alex321111@users.noreply.github.com>
2023-11-01 16:23:08 +0000

    backend deployment

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 
 const PORT = process.env.PORT ?? 3000;
+const HOST = process.env.HOST ?? "0.0.0.0";
 
-app.listen(PORT, function () {
+app.listen(PORT,HOST, function () {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
